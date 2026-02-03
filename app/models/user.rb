aa4ec263873f92bd3f0e87c811e5688a570ed090
HossainAraf <arafat.kd99@gmail.com>
2026-02-03 19:50:45 +0600

    refactor(User): remove attr_accessible and add strong parameters

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,6 @@ class User
   field :uid, type: String
   field :name, type: String
   field :email, type: String
-  attr_accessible :provider, :uid, :name, :email
   validates_presence_of :name
   # run 'rake db:mongoid:create_indexes' to create indexes
   index({ email: 1 }, { unique: true, background: true })
